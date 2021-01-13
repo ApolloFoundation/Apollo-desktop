ccb978a9240db013d79ae5b4e849fbd6a0ea7e1e
Rostyslav Golda <tx_hv@ukr.net>
2021-01-13 07:59:25 +0200

    Version Bump

--- apl-desktop/src/main/java/com/apollocurrency/aplwallet/apldesktop/Constants.java
+++ apl-desktop/src/main/java/com/apollocurrency/aplwallet/apldesktop/Constants.java
@@ -5,6 +5,6 @@ package com.apollocurrency.aplwallet.apldesktop;
 
 public final class Constants {
 
-    public static final String VERSION = "1.47.5";
+    public static final String VERSION = "1.47.6";
 
 }
