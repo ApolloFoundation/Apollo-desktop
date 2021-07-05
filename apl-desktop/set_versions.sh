9dd798a4cd53a8e9f263e92b6826a2c9ae18572b
Andrew Boyarsky <andrew.boyarski@gmail.com>
2021-07-05 17:13:05 +0300

    Add set_versions.sh script to update versions consistently, bump version to 1.47.19

---
+++ apl-desktop/set_versions.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Define versions
+if [ -z "${1}" ] ; then
+    NEW_VERSION=1.47.18
+else
+    NEW_VERSION=$1
+fi
+VF="VERSION-desktop"
+# set versions in parent pom and in all childs
+echo "New version is: $NEW_VERSION"
+echo $NEW_VERSION > $VF
+echo "Changing Maven's POM files"
+mvn versions:set -DnewVersion=${NEW_VERSION}
+
+PKG_PATH=packaging/pkg-apollo-desktop.json
+echo "Changing pkg-apollo-desktop.json"
+sed -i -e "s/\ \"version\".*/ \"version\": \"$NEW_VERSION\",/g" ${PKG_PATH}
