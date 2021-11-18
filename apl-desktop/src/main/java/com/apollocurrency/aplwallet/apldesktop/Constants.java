e2e3feda8d27994186cd328cc69e0c1501e8724f
Andrew Boyarsky <andrew.boyarski@gmail.com>
2021-11-18 10:58:50 +0200

    Bump version to 1.48.10

--- apl-desktop/src/main/java/com/apollocurrency/aplwallet/apldesktop/Constants.java
+++ apl-desktop/src/main/java/com/apollocurrency/aplwallet/apldesktop/Constants.java
@@ -5,6 +5,6 @@ package com.apollocurrency.aplwallet.apldesktop;
 
 public final class Constants {
 
-    public static final String VERSION = "1.48.9";
+    public static final String VERSION = "1.48.10";
 
 }
