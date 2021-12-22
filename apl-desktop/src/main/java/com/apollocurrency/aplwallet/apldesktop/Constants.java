a2ca6fbcc8f98b33a7d569760d10a33e7fd5e688
Rostyslav Golda <tx_hv@ukr.net>
2021-12-22 14:22:33 +0200

    Fixed typo

--- apl-desktop/src/main/java/com/apollocurrency/aplwallet/apldesktop/Constants.java
+++ apl-desktop/src/main/java/com/apollocurrency/aplwallet/apldesktop/Constants.java
@@ -5,6 +5,6 @@ package com.apollocurrency.aplwallet.apldesktop;
 
 public final class Constants {
 
-    public static final String VERSION = "1.50.32
+    public static final String VERSION = "1.50.32";
 
 }
