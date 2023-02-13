b67238231a237fbacb0a86bc39b255e5f7b97572
Yuriy Larin <blandger@gmail.com>
2023-02-13 18:31:40 +0200

    version bump to 2.0.1

--- apl-desktop/src/main/java/com/apollocurrency/aplwallet/apldesktop/Constants.java
+++ apl-desktop/src/main/java/com/apollocurrency/aplwallet/apldesktop/Constants.java
@@ -5,6 +5,6 @@ package com.apollocurrency.aplwallet.apldesktop;
 
 public final class Constants {
 
-    public static final String VERSION = "1.50.33";
+    public static final String VERSION = "2.0.1";
 
 }
